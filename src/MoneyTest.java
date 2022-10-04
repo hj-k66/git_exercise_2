ea80c490b2bcef194747acd5a3c7e7cf52d881e7
heejeong <hjx66@naver.com>
2022-10-04 14:26:16 +0900

    raw 코드로 몫과 나머지 구하기 - 50000~500까지 나눈 몫, 나머지

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -5,12 +5,18 @@ public class MoneyTest {
         Scanner sc = new Scanner(System.in);
         System.out.print("금액을 넣으세요:");
         int money = sc.nextInt();
-        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 
-        for(int i = 0; i < unit.length; ++i) {
-            int num = money / unit[i];
-            System.out.println(unit[i] + "원" + num + "개");
-            money -= unit[i] * num;
-        }
+
+        int curr50000 = 50000;
+        int curr10000 = 10000;
+        int curr5000 = 5000;
+        int curr1000 = 1000;
+        int curr500 = 500;
+
+        System.out.printf("50000원권 %d장, 나머지 : %d\n", (money/curr50000), money%curr50000);
+        System.out.printf("10000원권 %d장, 나머지 : %d\n", (money/curr10000), money%curr10000);
+        System.out.printf("5000원권 %d장, 나머지 : %d\n", (money/curr5000), money%curr5000);
+        System.out.printf("1000원권 %d장, 나머지 : %d\n", (money/curr1000), money%curr1000);
+        System.out.printf("500원권 %d장, 나머지 : %d\n", (money/curr500), money%curr500);
     }
 }
