00fbae7bef652e324a8ecdf199b7db534187fa70
heejeong <hjx66@naver.com>
2022-10-04 13:21:25 +0900

    Array 10개

---
+++ src/ArrayTest.java
@@ -0,0 +1,9 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for(int i = 0; i < 10; i++){
+            arr[i] = i+1;
+            System.out.println(arr[i]);
+        }
+    }
+}
