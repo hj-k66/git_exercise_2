60d18ea8dece7fd89a5409ac062f363352ed142e
heejeong <hjx66@naver.com>
2022-10-05 22:57:05 +0900

    [CodeUp] 1012 실수 1개 입력받아 그대로 출력하기

---
+++ src/CodeUp/C_1012.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.printf("%.6f",f);
+    }
+}
