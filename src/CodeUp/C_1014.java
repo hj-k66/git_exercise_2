b4991e250f8f689db3be56cd2bcdc6797d4b11e0
heejeong <hjx66@naver.com>
2022-10-05 23:07:22 +0900

    [CodeUp] 1014 문자 2개 입력받아 순서 바꿔 출력하기

---
+++ src/CodeUp/C_1014.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char x = sc.next().charAt(0);
+        char y = sc.next().charAt(0);
+        System.out.println(y + " " + x);
+    }
+}
