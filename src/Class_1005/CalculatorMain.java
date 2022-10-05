1680f739c86d2e7d5a9be7682a6ebfe6b222bd54
heejeong <hjx66@naver.com>
2022-10-05 10:17:44 +0900

    [add] Calculator 추가 - plus, minus, multiple, divide

---
+++ src/Class_1005/CalculatorMain.java
@@ -0,0 +1,11 @@
+package Class_1005;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.plus(1,2));
+        System.out.println(calculator.minus(10,1));
+        System.out.println(calculator.multiple(2,2));
+        System.out.println(calculator.divide(10,5));
+    }
+}
