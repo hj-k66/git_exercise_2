14961b8d1ab74569695e9a4cf19ae8aadf12624c
heejeong <hjx66@naver.com>
2022-10-05 23:11:55 +0900

    [CodeUp] 1017 정수 1개 입력받아 3번 출력하기

---
+++ src/CodeUp/C_1017.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%d %d %d", n,n,n);
+    }
+}
