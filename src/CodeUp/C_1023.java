55f21da7cf5e50ecd9a73726cd632bb9bcb1bce1
heejeong <hjx66@naver.com>
2022-10-06 20:37:46 +0900

    [CodeUp] 1023 실수 1개 입력받아 부분별로 출력하기

---
+++ src/CodeUp/C_1023.java
@@ -0,0 +1,16 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1023 {
+    public static void main(String[] args) {
+        //입력된 실수를 정수, 실수부로 나눠 출력
+        Scanner sc = new Scanner(System.in).useDelimiter("[.\n]");
+        int inum = sc.nextInt();
+        int fnum = sc.nextInt();
+
+        System.out.println(inum);
+        System.out.println(fnum);
+
+    }
+}
