9cd5a0660771f8fd4e04ba2db819353dad2c05e6
heejeong <hjx66@naver.com>
2022-10-06 21:12:24 +0900

    [CodeUp] 1030 정수 1개 입력받아 그대로 출력하기3

---
+++ src/CodeUp/C_1030.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        System.out.println(num);
+
+    }
+}
