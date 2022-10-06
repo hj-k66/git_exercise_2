1c2d47839b27ae524f656edc31b10a6b26b081a0
heejeong <hjx66@naver.com>
2022-10-06 21:10:25 +0900

    [CodeUp] 1029 실수 1개 입력받아 그대로 출력하기2

---
+++ src/CodeUp/C_1029.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        System.out.printf("%.11f", n);
+    }
+}
