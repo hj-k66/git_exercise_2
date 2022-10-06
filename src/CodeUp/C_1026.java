1a457807889b226519b78e20d37e5edc17c21c7c
heejeong <hjx66@naver.com>
2022-10-06 20:55:36 +0900

    [CodeUp] 1026 시분초 입력받아 분만 출력하기

---
+++ src/CodeUp/C_1026.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1026 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useDelimiter("[:\n]");
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+        int sec = sc.nextInt();
+        System.out.println(min);
+    }
+}
