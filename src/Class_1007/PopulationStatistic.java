57b83885db412a8e9666598a3808844395bdea72
heejeong <hjx66@naver.com>
2022-10-07 10:54:38 +0900

    [add] PopulationStatistic, PopluationMove 클래스 추가 - PopulationStatistic : 파일 읽는 메소드, parse메소드 - PopulationMove : 전입, 전출 관련 클래스

---
+++ src/Class_1007/PopulationStatistic.java
@@ -0,0 +1,69 @@
+package Class_1007;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class PopulationStatistic {
+    public void readByChar(String filename)throws  IOException{
+        FileReader fileReader = new FileReader(filename);
+        //파일을 읽지 않음
+
+        String fileContents ="";
+        while(fileContents.length()<1_000_000){
+            char c = (char)fileReader.read();
+            fileContents += c;
+            System.out.println(fileContents);
+        }
+        System.out.println(fileContents);
+
+    }
+
+    public void readByLine(String filename) throws  IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String str;
+        while((str= reader.readLine())!=null){
+            System.out.println(str);
+        }
+        reader.close();
+
+    }
+
+    public void readByLine2(String filename) {
+        try(BufferedReader br = Files.newBufferedReader(
+                Paths.get(filename), StandardCharsets.UTF_8)){
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public PopulationMove parse(String data){
+        String[] splitData = data.split(",");
+        int fromSido = Integer.valueOf(splitData[0]);
+        int toSido = Integer.valueOf(splitData[6]);
+        return new PopulationMove(fromSido, toSido);
+    }
+
+    public String readOneLine(String filename) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        return reader.readLine();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String address = "C:\\2021_인구관련연간자료_20221007_07638.csv";
+        PopulationStatistic populationStatistic = new PopulationStatistic();
+
+        String data = populationStatistic.readOneLine(address);
+        PopulationMove populationMove = populationStatistic.parse(data);
+        System.out.println("ToSido = " + populationMove.getToSido());
+        System.out.println("FromSido = " + populationMove.getFromSido());
+
+    }
+}
