046a9dd7cac1e16fe3b5f502e070528cb9571377
heejeong <hjx66@naver.com>
2022-10-10 15:19:09 +0900

    [CodeUp] 1037 정수 입력받아 아스키문자로 출력하기

---
+++ src/CodeUp/C_1037.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println((char)num);
+    }
+}
