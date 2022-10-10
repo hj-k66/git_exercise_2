dcb2bd9c1e8c0fba533da114752b0bf719376b0b
heejeong <hjx66@naver.com>
2022-10-10 14:52:34 +0900

    [CodeUp] 1031 10진 정수 1개 입력받아 8진수로 출력하기

---
+++ src/CodeUp/C_1031.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        //8진수 출력
+        System.out.printf("%o", num);
+    }
+}
