95a47465d68f28f8035be405a8553a94af9e8d4f
heejeong <hjx66@naver.com>
2022-10-10 15:17:09 +0900

    [CodeUp] 1036 영문자 1개 입력받아 10진수로 출력하기

---
+++ src/CodeUp/C_1036.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println((int)s.charAt(0));
+    }
+}
