a31b064b13c883c900b3a3150fe5a228514099b4
heejeong <hjx66@naver.com>
2022-10-10 15:00:25 +0900

    [CodeUp] 1033 10진 정수 입력받아 16진수로 출력하기2

---
+++ src/CodeUp/C_1033.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1033 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        //16진수 대문자로 출력
+        System.out.printf("%X", num);
+    }
+}
