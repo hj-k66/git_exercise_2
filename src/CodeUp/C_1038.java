20f433425e55b96d98af6cb31956c0b3360a141b
heejeong <hjx66@naver.com>
2022-10-10 15:24:47 +0900

    [CodeUp] 1038 정수 2개 입력받아 합 출력하기1

---
+++ src/CodeUp/C_1038.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1038 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n1 = sc.nextLong();
+        long n2 = sc.nextLong();
+        //n1,n2를 합하면 int범위를 넘어가므로 long
+        System.out.println(n1+n2);
+    }
+}
