fd516e1d7ec5fdb8c3ad337c33dd157ca46201af
heejeong <hjx66@naver.com>
2022-10-11 21:24:22 +0900

    [CodeUp] 1047 정수 1개 입력받아 2배 곱해 출력하기

---
+++ src/CodeUp/C_1047.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C_1047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n<<1);
+
+    }
+}
