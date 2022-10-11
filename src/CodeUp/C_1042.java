f2376d6750ffd1c7c90862136a49cf664f68e16b
heejeong <hjx66@naver.com>
2022-10-11 17:15:04 +0900

    [CodeUp] 1042 정수 2개 입력받아 나눈 몫 출력하기

---
+++ src/CodeUp/C_1042.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        System.out.println(n1/n2);
+    }
+}
