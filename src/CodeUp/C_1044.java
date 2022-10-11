bb76af5678907e7b41b529f4dc36d7c922f96572
heejeong <hjx66@naver.com>
2022-10-11 17:22:05 +0900

    [CodeUp] 1044 정수 1개 입력받아 1 더해 출력하기

---
+++ src/CodeUp/C_1044.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1044 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(++n);
+    }
+}
