e4bf9eacfed2f0429a06eaf69392fd515d66db25
heejeong <hjx66@naver.com>
2022-10-12 10:01:11 +0900

    Update:[CodeUp] 최댓값 1

--- src/week4/algorithm/Max1.java
+++ src/week4/algorithm/Max1.java
@@ -1,23 +1,25 @@
 package week4.algorithm;
 
 public class Max1 {
-    public int getMax(int[] arr){
+    // return : maxValue, maxIdx
+    public int[] getMax(int[] arr){
         //loop 구성
-        int maxValue = 0;
+        int maxValue = arr[0];
+        int maxIdx = 0;
         for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
             if(arr[i] > maxValue){
-                maxValue = arr[i]
+                maxValue = arr[i];
+                maxIdx = i;
             }
         }
-        return maxValue;
+        return new int[]{maxValue, maxIdx};
     }
     public static void main(String[] args) {
         int[] arr = new int[]{3, 29, 38, 12, 57, 74, 40, 85, 61 };
         Max1 max1 = new Max1();
-        int result = max1.getMax(arr);
+        int[] result = max1.getMax(arr);
 
-        if(result == 85){
+        if(result[0] == 85 && result[1] == 7){
             System.out.println("테스트 통과");
         }else{
             System.out.println("테스트 실패, result : " + result);
