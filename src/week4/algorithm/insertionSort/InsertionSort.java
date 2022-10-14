71de9236812c463806660660a2b7281bf370bf02
heejeong <hjx66@naver.com>
2022-10-14 09:45:43 +0900

    [add] swap 추가

--- src/week4/algorithm/insertionSort/InsertionSort.java
+++ src/week4/algorithm/insertionSort/InsertionSort.java
@@ -3,14 +3,19 @@ package week4.algorithm.insertionSort;
 import java.util.Arrays;
 
 public class InsertionSort {
-    public int[] sort(int[] arr){
+    public int[] sort(int[] arr) {
         int i = 1;
-
-        return new int[]{};
-
+        if (arr[i] < arr[i - 1]) {
+            int temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
+        return arr;
     }
+
+
     public static void main(String[] args) {
-        int[] arr = {8,5,6,2,4};
+        int[] arr = {8, 5, 6, 2, 4};
         InsertionSort is = new InsertionSort();
         int[] result = is.sort(arr);
         System.out.println(Arrays.toString(result));
