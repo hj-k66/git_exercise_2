9b2993073b8ecfd46960ba91650fc076cc4bd646
heejeong <hjx66@naver.com>
2022-10-14 10:25:19 +0900

    [CodeUp] 1443 삽입 정렬

---
+++ src/CodeUp/C_1443.java
@@ -0,0 +1,26 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C_1443 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
