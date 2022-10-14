9007730f9091e0615482b69cd503ecca54951907
heejeong <hjx66@naver.com>
2022-10-14 09:37:42 +0900

    [add] 삽입정렬 메소드 설계

---
+++ src/week4/algorithm/insertionSort/InsertionSort.java
@@ -0,0 +1,19 @@
+package week4.algorithm.insertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public int[] sort(int[] arr){
+        int i = 1;
+
+        return new int[]{};
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {8,5,6,2,4};
+        InsertionSort is = new InsertionSort();
+        int[] result = is.sort(arr);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
