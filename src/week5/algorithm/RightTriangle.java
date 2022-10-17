2a5e133f4dffa68bbc7275312b30a1dbe3c1650e
heejeong <hjx66@naver.com>
2022-10-17 09:22:30 +0900

    [refactor] n단 직각 삼각형 별찍기 - loop이용

--- src/week5/algorithm/RightTriangle.java
+++ src/week5/algorithm/RightTriangle.java
@@ -1,10 +1,29 @@
 package week5.algorithm;
 
+import java.util.Scanner;
+
 public class RightTriangle {
+    public void NRigntTriangle(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i+1; j++){
+                System.out.printf("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        /*
+        *   i = 0 별이 1개
+        **  i = 1 별이 2개
+        *** i = 2 별이 3개
+        **** i = 3 별이 4개
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.NRigntTriangle(n);
+
     }
 }
